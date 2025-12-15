b0df7e62dddf5eb955fdcc74f8b71a337bfbba29
Ajyothsna758 <jyoece758@gmail.com>
2025-12-15 14:30:46 +0000

    setup signin and signup app

--- userpanel/urls.py
+++ userpanel/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
 from . import views
 
+urlpatterns=[
+    path("", views.homelogin_view, name="home"),
+    path("sign-up/", views.signup_view, name="sign_up"),
+    path("logout", views.logout_view, name="logout"),
+    path("games/", views.games, name="games"),
+]
