28768911357e23e2dde06d5b56029b705e7dec61
Ajyothsna758 <jyoece758@gmail.com>
2025-12-21 04:05:37 +0000

    setup wishlist functionality

--- games/urls.py
+++ games/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("games/", views.games_list, name="games")
+    path("games/", views.games_list, name="games"),
+    path("wishlist/toggle/<int:game_id>", views.toggle_wishlist, name="wishlist_toggle"),
+    path("wishlist/", views.wishlist, name="wishlist"),
+    path("1/", views.check_svg),
 ]
--- games/views.py
+++ games/views.py
@@ -1,18 +1,48 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
 # Create your views here.
-from .models import Game, UserGameRating
+from .models import Game, UserGameRating, WishList
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def games_list(request):
     games= Game.objects.all()
+    # rating
     user_ratings={}
     if request.user.is_authenticated:
         user_ratings={
             r.game_id: r.rating for r in UserGameRating.objects.filter(user=request.user)
         }
-    return render(request, "games/games.html", {"games":games, "user_ratings":user_ratings})
+    # wishlist
+    wishlist_games=WishList.objects.filter(user=request.user).values_list("game_id", flat=True)
+        
+    return render(request, "games/games.html", 
+                  {"games":games, "user_ratings":user_ratings, 
+                   "wishlist_games": wishlist_games})
+   
+    
+# to add or remove game from wishlist page by clicking on image    
+@login_required
+def toggle_wishlist(request, game_id):
+    game= get_object_or_404(Game, id=game_id)
+    wishlist_game= WishList.objects.filter(user=request.user, game=game)
+    if wishlist_game.exists():
+        wishlist_game.delete()
+    else:
+        WishList.objects.create(user=request.user, game=game)       
+    return redirect(request.META.get("HTTP_REFERER", "games"))   
+ 
+@login_required
+def wishlist(request):
+    wishlist_items= WishList.objects.filter(user=request.user).select_related("game")
+    wishlist_games= wishlist_items.values_list("game_id", flat=True)
+    return render(request, "games/wishlist.html", 
+                  {"wishlist_games": wishlist_games,
+                   "wishlist_items":wishlist_items}) 
     
     
+    
+    
+@login_required
+def check_svg(request):
+    return render(request, "games/1.html")    
