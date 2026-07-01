051a375b25645ddc43407ed346d53283c1ab5c14
Ajyothsna758 <jyoece758@gmail.com>
2026-07-01 18:48:23 +0100

    added superuser

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,16 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
 
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+username = "admin"
+password = "shiva@1234"
+email = "admin@example.com"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+EOF
+
 exec "$@"
